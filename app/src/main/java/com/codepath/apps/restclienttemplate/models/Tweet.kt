5e5fd3e4322b8d46dd6f9699ab64d4dbdfde5478
Jesse Riley <jriley32@student.gsu.edu>
2022-03-13 23:57:22 -0400

    Use RecyclerView to implement timeline view

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -13,14 +13,14 @@ class Tweet {
         fun fromJson(jsonObject: JSONObject) : Tweet {
             val tweet = Tweet()
             tweet.body = jsonObject.getString("text")
-            tweet.createdAt = jsonObject.getString("createdAt")
+            tweet.createdAt = jsonObject.getString("created_at")
             tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
             return tweet
         }
 
         fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
             val tweets = ArrayList<Tweet>()
-            for (i in 0..jsonArray.length()) {
+            for (i in 0 until jsonArray.length()) {
                 tweets.add(fromJson(jsonArray.getJSONObject(i)))
             }
             return tweets
